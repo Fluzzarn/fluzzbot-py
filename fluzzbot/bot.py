9aae6dbe7d4a3f552bc0b83b9f26e05e35df33ca
fluzzarn <fluzzarn@gmail.com>
2018-09-26 20:19:08 -0400

    Added logging and config level channels to join

--- fluzzbot/bot.py
+++ fluzzbot/bot.py
@@ -1,10 +1,19 @@
 from python_twitch_irc import TwitchIrc
+import logging
 
 
 
 class FluzzBot(TwitchIrc):
+    joined_channels = ["fluzzbot"]
+    log = logging.getLogger(__name__)
+    def add_channels_to_join(self,channels):
+        self.joined_channels += channels
+        return self
+
     def on_connect(self):
-        self.join('#fluzzbot')
+        for channel in self.joined_channels:
+            self.log.info("JOINing " + channel)
+            self.join('#' + channel)
     
     def on_message(self,timestamp,tags,channel,user,message):
         print(tags)
--- fluzzbot/fluzzbot.py
+++ fluzzbot/fluzzbot.py
@@ -1,15 +1,20 @@
 import bot
 import configparser
 import json
+import os
+import logging
 
 def main():
 
+
+    logging.basicConfig(level=os.environ.get("LOGLEVEL","INFO"))
     config = configparser.ConfigParser()
     with open('config.json','r') as f:
         config = json.load(f)
     username = config['DEFAULT']["USERNAME"]
     password = config['DEFAULT']["PASSWORD"]
-    client = bot.FluzzBot(username,password).start()
+    channels = config['DEFAULT']["CHANNELS"]
+    client = bot.FluzzBot(username,password).add_channels_to_join(channels).start()
     client.handle_forever()
 
 
